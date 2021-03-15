c90af9e27b0d756bfb3a9e8b5193db5c5d7cfe06
Nikesh Ghimire <nghimir2@central.uh.edu>
2021-03-15 15:36:28 -0500

    Clean up

--- libs/products/ui-products/src/lib/ui-product-add/ui-product-add.component.ts
+++ libs/products/ui-products/src/lib/ui-product-add/ui-product-add.component.ts
@@ -2,21 +2,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'ui-ecommerce-product-add',
-  templateUrl: './ui-ecommerce-product-add.component.html',
-  styleUrls: ['ui-ecommerce-product-add.component.scss'],
+  templateUrl: './ui-product-add.component.html',
+  styleUrls: ['ui-product-add.component.scss'],
 })
-export class UiEcommerceProductAddComponent implements OnInit {
+export class UiProductAddComponent implements OnInit {
 
   /* When user clicks the cancel button on Add Product Form, it will emit the boolean (false).
   That will close the form. */
   @Output() cancelAddMode = new EventEmitter();
 
-
-  constructor() { }
-
   ngOnInit(): void {
   }
 
+  // Hide Add Product form when cancel button is clicked
   cancelAdd() {
     this.cancelAddMode.emit(false);
   }
--- libs/products/ui-products/src/lib/ui-products.module.ts
+++ libs/products/ui-products/src/lib/ui-products.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UiEcommerceProductAddComponent } from './ui-product-add/ui-ecommerce-product-add.component';
+import { UiProductAddComponent } from './ui-product-add/ui-product-add.component';
 import { UiEcommerceProductsListComponent } from './ui-products-list/ui-ecommerce-products-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [UiEcommerceProductAddComponent, UiEcommerceProductsListComponent],
-  exports: [UiEcommerceProductAddComponent, UiEcommerceProductsListComponent]
+  declarations: [UiProductAddComponent, UiEcommerceProductsListComponent],
+  exports: [UiProductAddComponent, UiEcommerceProductsListComponent]
 })
 export class UIProductsModule {}
